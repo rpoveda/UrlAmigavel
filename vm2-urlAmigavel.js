4b7c91821d27e9ab16a649463c211112b217dfe4
Kauê Gimenes <kauegimenes@users.noreply.github.com>
2015-02-12 09:04:47 -0200

    Update vm2-urlAmigavel.js

--- vm2-urlAmigavel.js
+++ vm2-urlAmigavel.js
@@ -1,15 +1,21 @@
 $.fn.urlAmigavel = function(valor){
-	var str_acento = "áàãâäéèêëíìîïóòõôöúùûüçÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÖÔÚÙÛÜÇ";
-	var str_sem_acento = "aaaaaeeeeiiiiooooouuuucAAAAAEEEEIIIIOOOOOUUUUC";
-	var url_amigavel = "";
-
-	for(var i = 0; i < valor.length; i++){
-		if(str_acento.indexOf(valor.charAt(i)) != -1){
-			url_amigavel += str_sem_acento.substr(str_acento.search(valor.substr(i,1)),1);
-		}else{
-			url_amigavel += valor.substr(i, 1);
-		}
+ 	var url_amigavel = valor;
+	var mapaAcentosHex = {
+		a : /[\xE0-\xE6]/g,
+		e : /[\xE8-\xEB]/g,
+		i : /[\xEC-\xEF]/g,
+		o : /[\xF2-\xF6]/g,
+		u : /[\xF9-\xFC]/g,
+		c : /\xE7/g,
+		n : /\xF1/g
+	};
+ 
+	for ( var letra in mapaAcentosHex ) {
+		var expressaoRegular = mapaAcentosHex[letra];
+		url_amigavel = url_amigavel.replace( expressaoRegular, letra );
 	}
+ 
 	url_amigavel = url_amigavel.replace(/ +/g, '-');
 	return url_amigavel;
 }
+
